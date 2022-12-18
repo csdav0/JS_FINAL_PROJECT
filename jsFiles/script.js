0366624bad48d104c79e42d19add523b0de638d4
Bryce Meadors <bryce.meadors@gmail.com>
2022-12-18 10:24:02 -0800

    Main character animations and movement. Added camera mechanics. Added attacking, rolling animations. Refactored codebase to be modular. Added music. Changed background image.

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -1,98 +1,18 @@
-var config = {
+import Scene from "./canvas_scene.js";
+
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  backgroundColor: "#000000",
+  parent: "game-container",
+  pixelArt: true,
+  scene: Scene,
   physics: {
     default: "arcade",
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
+    arcade: {
+      gravity: { y: 0 },
+    },
   },
 };
-let player;
-let controls;
-let game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("backgroundImage", "assets/backgroundImage.png");
-  /* knight is 10 columns x 24 rows, 32x32 bits */
-  this.load.spritesheet("knight", "assets/32bit-knight.png", {
-    frameWidth: 32,
-    frameHeight: 32,
-  });
-}
-
-function create() {
-  this.add.image(400, 300, "backgroundImage");
-  player = this.physics.add.sprite(400, 300, "knight");
-  player.setScale(2);
-  player.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: "idle",
-    frames: this.anims.generateFrameNumbers("knight", { start: 0, end: 3 }),
-    frameRate: 2,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("knight", { start: 10, end: 18 }),
-    frameRate: 4,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "up",
-    frames: this.anims.generateFrameNumbers("knight", { start: 20, end: 28 }),
-    frameRate: 4,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("knight", { start: 30, end: 38 }),
-    frameRate: 4,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "down",
-    frames: this.anims.generateFrameNumbers("knight", { start: 40, end: 48 }),
-    frameRate: 4,
-    repeat: -1,
-  });
-
-  controls = this.input.keyboard.createCursorKeys();
-}
-
-let speed = 100;
-function update() {
-  if (controls.left.isDown) {
-    player.setVelocityX(-speed);
-    player.setVelocityY(0);
-    player.anims.play("left", true);
-
-  } else if (controls.right.isDown) {
-    player.setVelocityX(speed);
-    player.setVelocityY(0);
-    player.anims.play("right", true);
-
-  } else if (controls.up.isDown) {
-    player.setVelocityY(-speed);
-    player.setVelocityX(0);
-    player.anims.play("up", true);
-    
-  } else if (controls.down.isDown) {
-    player.setVelocityY(speed);
-    player.setVelocityX(0);
-    player.anims.play("down", true);
-    
-  } else {
-    player.setVelocityX(0);
-    player.setVelocityY(0);
-    player.anims.play("idle", true);
-  }
-}
+const game = new Phaser.Game(config);
