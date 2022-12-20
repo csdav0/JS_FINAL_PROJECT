467180a0a20889a09f0ce54b847551c554b661cb
Cameron Davis <csd@fastmail.com>
2022-12-20 11:12:44 -0500

    swapping to manually created scene

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -1,4 +1,24 @@
 import DungeonScene from "./dungeon-gen.js";
+import StartScene from "./start-scene.js";
+
+// const config = {
+//   type: Phaser.AUTO,
+//   width: 800,
+//   height: 600,
+//   backgroundColor: "#000",
+//   parent: "container",
+//   pixelArt: true,
+//   scene: DungeonScene,
+//   physics: {
+//     default: "arcade",
+//     arcade: {
+//       gravity: { y: 0 },
+//       debug: false,
+//     },
+//   },
+// };
+
+// const game = new Phaser.Game(config);
 
 const config = {
   type: Phaser.AUTO,
@@ -7,7 +27,7 @@ const config = {
   backgroundColor: "#000",
   parent: "container",
   pixelArt: true,
-  scene: DungeonScene,
+  scene: StartScene,
   physics: {
     default: "arcade",
     arcade: {
